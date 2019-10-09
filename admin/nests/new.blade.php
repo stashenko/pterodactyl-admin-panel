4c0cae7026297fd3f9af427ddf9d94fb23ea0183
stashenko <34498917+stashenko@users.noreply.github.com>
2019-10-09 19:22:10 +0500

    Update new.blade.php

--- admin/nests/new.blade.php
+++ admin/nests/new.blade.php
@@ -6,15 +6,15 @@
 @extends('layouts.admin')
 
 @section('title')
-    New Nest
+    Новое Гнездо
 @endsection
 
 @section('content-header')
-    <h1>New Nest<small>Configure a new nest to deploy to all nodes.</small></h1>
+    <h1>Новое Гнездо<small>Настройте новое гнездо для развертывания на всех узлах.</small></h1>
     <ol class="breadcrumb">
-        <li><a href="{{ route('admin.index') }}">Admin</a></li>
-        <li><a href="{{ route('admin.nests') }}">Nests</a></li>
-        <li class="active">New</li>
+        <li><a href="{{ route('admin.index') }}">Администратор</a></li>
+        <li><a href="{{ route('admin.nests') }}">Гнезда</a></li>
+        <li class="active">Новое</li>
     </ol>
 @endsection
 
@@ -24,18 +24,18 @@
         <div class="col-md-12">
             <div class="box">
                 <div class="box-header with-border">
-                    <h3 class="box-title">New Nest</h3>
+                    <h3 class="box-title">Новое Гнездо</h3>
                 </div>
                 <div class="box-body">
                     <div class="form-group">
-                        <label class="control-label">Name</label>
+                        <label class="control-label">Название</label>
                         <div>
                             <input type="text" name="name" class="form-control" value="{{ old('name') }}" />
-                            <p class="text-muted"><small>This should be a descriptive category name that encompasses all of the eggs within the nest.</small></p>
+                            <p class="text-muted"><small>Это должно быть описательным названием категории, которая охватывает все яйца в гнезде.</small></p>
                         </div>
                     </div>
                     <div class="form-group">
-                        <label class="control-label">Description</label>
+                        <label class="control-label">Опимание</label>
                         <div>
                             <textarea name="description" class="form-control" rows="6">{{ old('description') }}</textarea>
                         </div>
@@ -43,7 +43,7 @@
                 </div>
                 <div class="box-footer">
                     {!! csrf_field() !!}
-                    <button type="submit" class="btn btn-primary pull-right">Save</button>
+                    <button type="submit" class="btn btn-primary pull-right">Сохранить</button>
                 </div>
             </div>
         </div>
